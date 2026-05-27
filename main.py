1f78e7320567a4d2885c257ff3ff2d0a47faf349
Giovanni Confente Broll Avila <gconfentebrollavila@fbk.eu>
2026-05-27 16:52:06 +0200

    Added streamlit-webrtc library, streamlit theme. Updated .gitignore and requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import av
+from streamlit_webrtc import webrtc_streamer
 
 ####################################
 # Variables
 ####################################
 if "ctr" not in st.session_state:
     st.session_state["ctr"] = 0
+if "is_transcripted" not in st.session_state:
+    st.session_state["is_transcripted"] = False
 if "lang" not in st.session_state:
     st.session_state['lang'] = "Italian"
 AUDIO_DIR = "./user_files"
@@ -32,6 +36,16 @@ st.markdown("""
 # Functions
 ####################################
 
+def audio_frame_callback(frame: av.AudioFrame) -> av.AudioFrame: 
+    # Apply volume control to audio samples
+    samples = frame.to_ndarray()
+    samples = (volume * samples).astype(samples.dtype)  # type: ignore
+    # Create new frame with processed audio
+    new_frame = av.AudioFrame.from_ndarray(samples, layout=frame.layout.name)
+    new_frame.sample_rate = frame.sample_rate
+        
+    return new_frame
+
 def save_file(file):
     if file is not None:
         bytes_data = file.getvalue()
@@ -41,56 +55,76 @@ def save_file(file):
 
 # UI
 st.title(""":blue[Transcriptor]""")
+mic_tab, file_tab = st.tabs(["Microfono", "Upload file"])
 
-with st.container(
-    border=True, height="stretch", width="stretch", horizontal_alignment="center"
-):
-    with st.container(horizontal_alignment="center"):
-        chosen_mode = st.selectbox("""**:blue[Scegli la modalità]**""", ("File", "Microfono"), label_visibility="collapsed")
-    
-    st.divider()
-    # with st.container(border=True, height="content", width="stretch"):
-    # File upload
-    if chosen_mode == "File":
+with mic_tab:
+    with st.container(
+        border=True, height="stretch", width="stretch", horizontal_alignment="center"
+    ):
+        
+        volume = st.slider("Volume", 0.0, 2.0, 1.0, 0.1)
+        webrtc_streamer(    
+            key="audio",
+            audio_frame_callback=audio_frame_callback,
+            media_stream_constraints={"video": False, "audio": True},
+        )
+
+
+        st.divider()
+
+        with st.container(horizontal=True, horizontal_alignment="distribute"):
+            with st.popover("Settings"):
+                # Scelta del linguaggio
+                st.session_state.lang = st.selectbox("Seleziona lingua", key="mic_chosen_lang", options=["Italian", "English"])
+
+                # Scelta della task
+                task_scelta = st.radio("Seleziona Task", key="mic_chosen_task", options=["ASR", "ST"])
+
+                # Scelta dell'uscita
+                uscita_scelta = st.selectbox(
+                    "Scegli l'uscita", key="mic_chosen_exit", options=["1", "2", "3", "4", "5", "6"]
+                )
+
+        st.divider()
+        st.write("Trascrizione...")
+
+with file_tab:
+    with st.container(
+        border=True, height="stretch", width="stretch", horizontal_alignment="center"
+        ):
+        # File upload
         uploaded_file = st.file_uploader(
             "Upload file", type="audio", label_visibility="collapsed"
         )
         file_to_transcript = uploaded_file
         st.session_state.is_transcripted = False
-    # Mic rec
-    else:
-        audio_batch = st.audio_input("Insert audio", label_visibility="collapsed")
-        file_to_transcript = audio_batch
 
-    st.divider()
+        st.divider()
 
-    with st.container(horizontal=True, horizontal_alignment="distribute"):
-        with st.popover("Settings"):
-            # Scelta del linguaggio
-            st.session_state.lang = st.selectbox("Seleziona lingua", ["Italian", "English"])
+        with st.container(horizontal=True, horizontal_alignment="distribute"):
+            with st.popover("Settings"):
+                # Scelta del linguaggio
+                st.session_state.lang = st.selectbox("Seleziona lingua", ["Italian", "English"])
 
-            # Scelta della task
-            task_scelta = st.radio("Seleziona Task", ["ASR", "ST"])
+                # Scelta della task
+                task_scelta = st.radio("Seleziona Task", ["ASR", "ST"])
 
-            # Scelta del modello
-            modello_scelto = st.selectbox(
-                "Scegli il modello", options=["M1", "M2", "M3", "M4"]
-            )
+                # Scelta dell'uscita
+                uscita_scelta = st.selectbox(
+                    "Scegli l'uscita", options=["1", "2", "3", "4", "5", "6"]
+                )
 
-        # Si procede a trascrivere
-        with st.container(horizontal_alignment="center", width="content"):
-            if chosen_mode == "File":
+            # Si procede a trascrivere
+            with st.container(horizontal_alignment="center", width="content"):
                 if st.button("Trascrivi", type="tertiary"):
                     if file_to_transcript is not None:
                         save_file(file_to_transcript)
                         # Chiamare logica di trascrizione
                         st.session_state.is_transcripted = True
-                    else:
-                        st.warning("Inserisci qualcosa da trascrivere")
 
-    st.divider()
+        st.divider()
 
-    if chosen_mode == "Microfono" and file_to_transcript is not None:
-        st.html("<p>Qua apparira' la trascrizione</p>")
-    elif chosen_mode == "File" and file_to_transcript is not None:
-        st.write("Trascrizione")
+        if file_to_transcript is not None and st.session_state.is_transcripted:
+            st.write("Trascrizione")
+        else:
+            st.error("Nulla da trascrivere")
