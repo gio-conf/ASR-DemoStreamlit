987d4ab9b2d9ef94e58fbbe6f4b4f2d47f663b55
Giovanni Confente Broll Avila <gconfentebrollavila@fbk.eu>
2026-06-07 16:09:14 +0200

    WIP

---
+++ frontend/main.py
@@ -0,0 +1,340 @@
+import streamlit as st
+import av
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
+import requests
+import webrtcvad
+import queue
+import threading
+import numpy as np
+import time
+from scipy.signal import resample
+
+SENTINEL = "STOP"
+
+@st.cache_resource
+def get_audio_queue():
+    return queue.Queue()
+
+@st.cache_resource
+def get_update_queue():
+    return queue.Queue()
+
+@st.cache_resource
+def get_finish_queue():
+    return queue.Queue()
+
+update_queue = get_update_queue()
+audio_queue = get_audio_queue()
+finish_queue = get_finish_queue()
+
+if 'finished' not in st.session_state:
+    st.session_state['finished'] = False
+
+def audio_frame_callback(frame: av.AudioFrame):
+    audio = frame.to_ndarray()
+
+    if frame.layout.name == "stereo":
+        audio = audio.reshape(-1, 2)
+        audio = audio.mean(axis=1)
+
+    num_samples = audio.shape[0]
+    target_num_samples = int(num_samples * 16000 / 48000)
+
+    audio = resample(audio, target_num_samples)
+    audio = audio.astype(np.int16)
+    audio_queue.put(audio)
+
+    return frame
+
+def on_audio_ended():
+    get_audio_queue().put(SENTINEL)
+    get_finish_queue().put(True)
+
+def sender_worker(audio_queue):
+    vad = webrtcvad.Vad(2)
+    URL = "http://127.0.0.1:8000/chunks/"
+    session_id = None
+    f = None
+    while True:
+        data = audio_queue.get()
+        if isinstance(data, str) and data == SENTINEL:
+            print("sentinel recv")
+            if f is not None:
+                f.close()
+                f = None
+            print("stopped sending")
+            continue
+
+        audio_16 = data
+
+        is_speech = vad.is_speech(audio_16, 16000)
+
+        raw_data = audio_16.tobytes()        
+
+        if is_speech:
+            if f is None:
+                f = open("out.raw", "wb")
+            f.write(raw_data)
+            f.flush()
+
+            files = {
+                "file": (
+                    "microfono.part0",
+                    raw_data,
+                    "application/octet-stream",
+                )
+            }
+
+            params = {}
+
+            if session_id:
+                params["session_id"] = session_id
+
+            response = requests.post(
+                URL,
+                files=files,
+                data=params,
+            )
+
+            resp_json = response.json()
+            session_id = resp_json["session_id"]
+            if resp_json["text"]:
+                update_queue.put(resp_json)
+
+if "worker_thread" not in st.session_state:
+    print("starting new thread")
+    t = threading.Thread(target=sender_worker, args=(audio_queue,), daemon=True)
+    t.start()
+    st.session_state.worker_thread = t
+
+
+####################################
+# Variables
+####################################
+if "ctr" not in st.session_state:
+    st.session_state["ctr"] = 0
+if "is_transcripted" not in st.session_state:
+    st.session_state["is_transcripted"] = False
+if "lang" not in st.session_state:
+    st.session_state['lang'] = "Italian"
+if 'is_transcripted' not in st.session_state:
+    st.session_state['is_transcripted'] = False
+if 'transcripted_text' not in st.session_state:
+    st.session_state['transcripted_text'] = ""
+if 'is_model_loaded' not in st.session_state:
+    st.session_state['is_model_loaded'] = False
+if 'exit' not in st.session_state:
+    st.session_state['exit'] = "All"
+if 'realtime_content' not in st.session_state:
+    st.session_state['realtime_content'] = ""
+
+AUDIO_DIR = "user_files/"
+file_to_transcript = ""
+
+
+####################################
+# CSS Styling
+####################################
+
+st.markdown("""
+<style>
+    h1 {
+        font-size: 24px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+####################################
+# Functions
+####################################
+
+# UI
+st.title("Transcriber")
+
+# Sidebar
+if not st.session_state['is_model_loaded']:
+    with st.sidebar:
+        with st.container(horizontal_alignment='center', horizontal=False):
+            st.session_state.lang = st.selectbox("Select language", key="model_lang", options=["Italian", "English"])
+            if st.button("Load model"):
+                try:
+                    resp = requests.post("http://127.0.0.1:8000/set_model/", data={'lang': st.session_state.lang})
+                    st.session_state['is_model_loaded'] = True
+                except:
+                    st.error("Server Error")
+
+# TABS
+mic_file_tab, file_tab, mic_rt_tab  = st.tabs(["Record & Transcribe", "Upload & Transcribe", "Real-Time transcription"])
+
+with mic_rt_tab:
+    with st.container(
+        border=True, height="stretch", width="stretch", horizontal_alignment="center"
+    ):
+        if st.session_state['is_model_loaded']:
+            ctx = webrtc_streamer(    
+                key="audio",
+                audio_frame_callback=audio_frame_callback,
+                on_audio_ended=on_audio_ended,
+                mode=WebRtcMode.SENDONLY,
+                media_stream_constraints={"video": False, "audio": True},
+            )
+
+            st.divider()
+            with st.container(horizontal=True, horizontal_alignment="distribute"):
+                with st.popover("Settings"):
+                    st.write("WIP")
+                    # Scelta del linguaggio
+                    # st.session_state.lang = st.selectbox("Seleziona lingua", key="mic_rt_chosen_lang", options=["Italian", "English"])
+
+                    # Scelta della task
+                    # task_scelta = st.radio("Seleziona Task", key="mic_rt_chosen_task", options=["ASR"])
+
+                    # Scelta dell'uscita
+                    # st.session_state.exit = st.selectbox(
+                    #     "Scegli l'uscita", key="mic_rt_chosen_exit", options=["All", "1", "2", "3", "4", "5", "6"]
+                    # )
+
+            st.divider()
+            history_box = st.empty()
+
+            transcript = ""
+
+            poll_interval = 0.2
+            while ctx.state.playing:
+                try:
+                    while True:
+                        resp_json = update_queue.get_nowait()
+                        text = resp_json.get("text", "")
+                        if text:
+                            transcript = text
+                            history_box.write(transcript)
+                            st.session_state['realtime_content'] = transcript
+                except queue.Empty:
+                    pass
+
+                time.sleep(poll_interval)
+
+            st.divider()
+
+            try:
+                st.session_state['finished'] = finish_queue.get_nowait()
+            except queue.Empty:
+                pass
+
+            if st.session_state['finished']:
+                print(f'{st.session_state["realtime_content"]=}')
+                st.write(st.session_state['realtime_content'])
+
+        else:
+            st.warning("Carica il modello dalla sidebar")
+
+with file_tab:
+    with st.container(
+        border=True, height="stretch", width="stretch", horizontal_alignment="center"
+        ):
+
+        if st.session_state['is_model_loaded']:
+            # File upload
+            uploaded_file = st.file_uploader(
+                "Upload file", type=[".wav", ".ogg", '.flac'], label_visibility="collapsed"
+            )
+
+            st.session_state.is_transcripted = False
+
+            st.divider()
+
+            with st.container(horizontal=True, horizontal_alignment="distribute"):
+                with st.popover("Settings"):
+                    # Scelta del linguaggio
+                    st.session_state.lang = st.selectbox("Select language", ["Italian", "English"])
+
+                    # Scelta della task
+                    task_scelta = st.radio("Select Task", ["ASR"])
+
+                    # Scelta dell'uscita
+                    st.session_state.exit = st.selectbox(
+                        "Select exit", options=["All", "1", "2", "3", "4", "5", "6"]
+                    )
+
+                # Si procede a trascrivere
+                with st.container(horizontal_alignment="center", width="content"):
+                    if st.button("Transcribe", type="tertiary"):
+                        file_to_transcript = uploaded_file
+                        file_to_transcript.name = "user_uploaded_file.wav"
+                        if file_to_transcript is not None:
+                            files = {"file": (file_to_transcript.name, file_to_transcript, "audio/wav")}
+                            try:
+                                resp = requests.post("http://127.0.0.1:8000/uploads", files=files)
+                                st.session_state['transcripted_text'] = resp.json()['text']
+                                st.session_state.is_transcripted = True
+                            except ConnectionError:
+                                st.error("Server irraggiungibile")
+                        else:
+                            st.error("Nulla da trascrivere")
+
+            st.divider()
+
+            if file_to_transcript is not None and st.session_state.is_transcripted:
+                if st.session_state['exit'] == 'All':
+                    for i in range(len(st.session_state['transcripted_text'])):
+                        st.write(f'Exit {i+1}: ' + st.session_state['transcripted_text'][i])
+                else:
+                    chosen_idx = int(st.session_state['exit']) - 1
+                    st.write(f'Exit {chosen_idx+1}: ' + st.session_state['transcripted_text'][chosen_idx])
+        else:
+            st.warning("Carica il modello dalla sidebar")    
+
+with mic_file_tab:
+    with st.container(
+        border=True, height="stretch", width="stretch", horizontal_alignment="center"
+        ):
+
+        if st.session_state['is_model_loaded']:
+            file = st.audio_input("Audio", label_visibility="collapsed", sample_rate=16000)
+            st.session_state.is_transcripted = False
+
+            st.divider()
+
+            with st.container(horizontal=True, horizontal_alignment="distribute"):
+                with st.popover("Settings"):
+                    # Scelta del linguaggio
+                    st.session_state.lang = st.selectbox("Select language", key='mic_file_chosen_lang', options=["Italian", "English"])
+
+                    # Scelta della task
+                    task_scelta = st.radio("Select Task", key="mic_lang_chosen_task", options=["ASR"])
+
+                    # Scelta dell'uscita
+                    st.session_state.exit = st.selectbox(
+                        "Select exit", key="mic_file_chosen_exit", options=["All", "1", "2", "3", "4", "5", "6"]
+                    )
+
+                # Si procede a trascrivere
+                with st.container(horizontal_alignment="center", width="content"):
+                    if st.button("Transcribe", key='mic_file_transcribe_btn', type="tertiary"):
+                        file_to_transcript = file
+                        if file_to_transcript is not None:
+                            file_to_transcript.name = "user_file.wav"
+                            files = {"file": (file_to_transcript.name, file_to_transcript, "audio/wav")}
+                            resp = requests.post("http://127.0.0.1:8000/uploads/", files=files)
+                            st.session_state['transcripted_text'] = resp.json()['text']
+                            st.session_state.is_transcripted = True
+                        else:
+                            st.error("Nulla da trascrivere")
+
+            st.divider()
+
+            if file_to_transcript is not None and st.session_state.is_transcripted:
+                if st.session_state['exit'] == 'All':
+                    for i in range(len(st.session_state['transcripted_text'])):
+                        st.write(f'Exit {i+1}: ' + st.session_state['transcripted_text'][i])
+                else:
+                    chosen_idx = int(st.session_state['exit']) - 1
+                    st.write(f'Exit {chosen_idx+1}: ' + st.session_state['transcripted_text'][chosen_idx])
+        else:
+            st.warning("Carica il modello dalla sidebar")
+
+
+# st.caption("Made by Giovanni Confente Broll Avila")
+        
